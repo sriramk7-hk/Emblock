81c1a57b469f59473355f10d39669e42a5f459e4
Thivin02 <erthivin.galado02@gmail.com>
2023-07-17 19:33:38 +0530

    footer responsiveness modified

--- frontend/emblock.in/src/Footer.jsx
+++ frontend/emblock.in/src/Footer.jsx
@@ -1,53 +1,107 @@
-import React from 'react'
-import "./assets/css/footer.css"
-import _ from "lodash" 
-import { Divider, Grid, Icon, List } from "semantic-ui-react"
+import React from "react";
+import "./assets/css/footer.css";
+import _ from "lodash";
+import { Divider, Grid, Icon, List } from "semantic-ui-react";
 
 function Footer() {
-  
-  const socialIcons = ["linkedin", "facebook square", "discord", "instagram", "youtube"];
-  
+  const socialIcons = [
+    "linkedin",
+    "facebook square",
+    "discord",
+    "instagram",
+    "youtube",
+  ];
+
   return (
     <footer className="footerPos">
-        <Divider inverted horizontal style={{marginTop: "3rem"}}><div className="footerTitle">Emblock</div></Divider>
-        <p className='footerMessage'>We deliver hight quality product effectively with your preferred time</p>
-        <Grid divided columns={3}>
-            <Grid.Column className='footerMessage'>
-                <h4>Contact</h4>
-                <List>
-                    <List.Item content={<p><Icon name="marker"/>Chennai,IN</p>}/>
-                    <List.Item content={<a><Icon name="mail"/>contact@emblock.in</a>}/>
-                    <List.Item content={<p><Icon name="phone"/>1234567890</p>}/>
-                </List>
-            </Grid.Column>
-            <Grid.Column className='footerMessage'>
-                <h4>Services</h4>
-                <List>
-                    <List.Item content={<a>IOT&nbsp;Blockchain&nbsp;Integration</a>}/>
-                    <List.Item content={<a>Development&nbsp;of&nbsp;Dapp</a>}/>
-                    <List.Item content={<a>Development&nbsp;of&nbsp;websites&nbsp;in&nbsp;web2&nbsp;and&nbsp;web3</a>}/>
-                    <List.Item content={<a>Seamless&nbsp;Integration&nbsp;of&nbsp;Embedded&nbsp;products</a>}/>
-                </List>
-            </Grid.Column>
-            <Grid.Column className='footerMessage'>
-                <h4>About</h4>
-                <List>
-                    <List.Item content={<a>Who&nbsp;are&nbsp;we?</a>}/>
-                    <List.Item content={<a>Meet&nbsp;our&nbsp;team</a>}/>
-                    <List.Item content={<a>Our&nbsp;future&nbsp;products</a>}/>
-                </List>
-            </Grid.Column>
-            <Grid.Row centered columns={5}>
-                    <a><Icon name="linkedin" size='large'/></a>
-                    <a><Icon name="facebook" size='large'/></a>
-                    <a><Icon name="discord" size='large'/></a>
-                    <a><Icon name="instagram" size='large'/></a>
-                    <a><Icon name="youtube" size='large'/></a>
-            </Grid.Row>
-        </Grid>
-        <p className='footerMessageCopy'>Copyrights @ 2023 Emblock All Rights Reserved&nbsp;&nbsp;</p>
+      <Divider inverted horizontal style={{ marginTop: "3rem" }}>
+        <div className="footerTitle">Emblock</div>
+      </Divider>
+      <p className="footerMessage">
+        We deliver hight quality product effectively with your preferred time
+      </p>
+      <Grid divided columns={3} className="reply">
+        <div className="footerMessage">
+          <List>
+            <h4>Contact</h4>
+            <List.Item
+              content={
+                <p>
+                  <Icon name="marker" />
+                  Chennai,IN
+                </p>
+              }
+            />
+            <List.Item
+              content={
+                <a>
+                  <Icon name="mail" />
+                  contact@emblock.in
+                </a>
+              }
+            />
+            <List.Item
+              content={
+                <p>
+                  <Icon name="phone" />
+                  1234567890
+                </p>
+              }
+            />
+          </List>
+        </div>
+        <div className="footerMessage">
+          <List>
+            <h4>Services</h4>
+            <List.Item content={<a>IOT&nbsp;Blockchain&nbsp;Integration</a>} />
+            <List.Item content={<a>Development&nbsp;of&nbsp;Dapp</a>} />
+            <List.Item
+              content={
+                <a>
+                  Development&nbsp;of&nbsp;websites&nbsp;in&nbsp;web2&nbsp;and&nbsp;web3
+                </a>
+              }
+            />
+            <List.Item
+              content={
+                <a>
+                  Seamless&nbsp;Integration&nbsp;of&nbsp;Embedded&nbsp;products
+                </a>
+              }
+            />
+          </List>
+        </div>
+        <div className="footerMessage">
+          <List>
+            <h4>About</h4>
+            <List.Item content={<a>Who&nbsp;are&nbsp;we?</a>} />
+            <List.Item content={<a>Meet&nbsp;our&nbsp;team</a>} />
+            <List.Item content={<a>Our&nbsp;future&nbsp;products</a>} />
+          </List>
+        </div>
+        <Grid.Row centered columns={5}>
+          <a>
+            <Icon name="linkedin" size="large" />
+          </a>
+          <a>
+            <Icon name="facebook" size="large" />
+          </a>
+          <a>
+            <Icon name="discord" size="large" />
+          </a>
+          <a>
+            <Icon name="instagram" size="large" />
+          </a>
+          <a>
+            <Icon name="youtube" size="large" />
+          </a>
+        </Grid.Row>
+      </Grid>
+      <p className="footerMessageCopy">
+        Copyrights @ 2023 Emblock All Rights Reserved&nbsp;&nbsp;
+      </p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/emblock.in/src/NavBar.jsx
+++ frontend/emblock.in/src/NavBar.jsx
@@ -1,6 +1,6 @@
-import { Grid, Menu } from "semantic-ui-react" 
-import './assets/css/NavBar.css'
-import { useState } from "react"
+import { Grid, Menu } from "semantic-ui-react";
+import "./assets/css/NavBar.css";
+import { useState } from "react";
 import Home from "./Home";
 import Projects from "./Projects";
 import About from "./About";
@@ -9,36 +9,54 @@ function NavBar() {
   const [navItem, setNavItem] = useState("home");
 
   const handleNavClick = (nav, e) => {
-    e.preventDefault()
-    if(nav === 1){
-        setNavItem("home")
-    }else if(nav === 2){
-        setNavItem("projects")
-    }else if(nav === 3){
-        setNavItem("about")
-    }else{
-        return;
+    e.preventDefault();
+    if (nav === 1) {
+      setNavItem("home");
+    } else if (nav === 2) {
+      setNavItem("projects");
+    } else if (nav === 3) {
+      setNavItem("about");
+    } else {
+      return;
     }
-  }
+  };
 
   return (
     <div className="nav-bar">
-    <Grid>
+      <Grid>
         <Grid.Row className="navbarStyle">
-      <Menu secondary className="menu">  
-        <Menu.Item active={navItem === "home"} onClick={(e) => handleNavClick(1, e)} className="nav-text">Home</Menu.Item>
-        <Menu.Item active={navItem === "projects"} onClick={(e) => handleNavClick(2, e)} className="nav-text">Projects</Menu.Item>
-        <Menu.Item active={navItem === "about"} onClick={(e) => handleNavClick(3, e)} className="nav-text">About</Menu.Item>   
-      </Menu>
-      </Grid.Row>
-        <Grid.Row>
-        {navItem === "home" ? <Home /> : ""}
-        {navItem === "projects" ? <Projects /> : ""}
-        {navItem === "about" ? <About /> : ""}
+          <Menu secondary className="menu">
+            <Menu.Item
+              active={navItem === "home"}
+              onClick={(e) => handleNavClick(1, e)}
+              className="nav-text"
+            >
+              Home
+            </Menu.Item>
+            <Menu.Item
+              active={navItem === "projects"}
+              onClick={(e) => handleNavClick(2, e)}
+              className="nav-text"
+            >
+              Projects
+            </Menu.Item>
+            <Menu.Item
+              active={navItem === "about"}
+              onClick={(e) => handleNavClick(3, e)}
+              className="nav-text"
+            >
+              About
+            </Menu.Item>
+          </Menu>
         </Grid.Row>
-    </Grid>
+        <Grid.Row>
+          {navItem === "home" ? <Home /> : ""}
+          {navItem === "projects" ? <Projects /> : ""}
+          {navItem === "about" ? <About /> : ""}
+        </Grid.Row>
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
