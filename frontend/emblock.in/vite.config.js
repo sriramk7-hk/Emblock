2db5fabb07cf5f4e5a8cb57b28f6e04bf34024a2
Sriram Kesavan <sriramkesavan44@gmail.com>
2023-07-30 19:54:10 +0530

    Build

--- frontend/emblock.in/vite.config.js
+++ frontend/emblock.in/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Emblock/frontend/emblock.in/"
-})
+  base: "/Emblock/",
+
+  build: {
+    outDir: "/Emblock/frontend/emblock.in/build",
+  },
+});
